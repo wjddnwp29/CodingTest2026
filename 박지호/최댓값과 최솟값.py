0eb61f7987bc78581d243cda5d55ffe859bc2f17
Jiho Park <andrew0305@naver.com>
2026-03-31 11:11:20 +0900

    최댓값과 최솟값

--- 박지호/최댓값과 최솟값.py
+++ 박지호/최댓값과 최솟값.py
@@ -1,6 +1,4 @@
 def solution(s):
-    k = s.split(' ')
-    for i in range(len(k)):
-        k[i] = k[i].capitalize()
-    answer = ' '.join(k)
+    k = list(map(int,s.split()))
+    answer = f"{min(k)} {max(k)}"
     return answer
