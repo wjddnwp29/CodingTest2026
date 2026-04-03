96f95af0dfb480b8952e5006612ed30be1383b75
P-JiHo77 <P-JiHo77>
2026-04-03 09:30:59 +0900

    0403

--- 박지호/짝지어 제거.py
+++ 박지호/짝지어 제거.py
@@ -1,14 +1,12 @@
 def solution(s):
-    i=0
-    while i < len(s) - 1:
-        if s[i] == s[i+1]:
-            s = s[:i] + s[i+2:]
-            i=0
-        else : 
-            i+=1
+    stack = []
+    
+    for c in s:
+        if stack and stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
 
-    if not s : k=1
-    else : k=0
-    return k
+    if not stack: return 1
+    else: return 0
 
-print(solution("cdcd"))
