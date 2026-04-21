f3c87eb0c59f6b0cdba9c8933714543b4a0f9473
정우제 <dnwpgmlfk@naver.com>
2026-04-21 16:41:57 +0900

    add : 가장 큰 수

---
+++ 정우제/Lv2/가장 큰 수.py
@@ -0,0 +1,13 @@
+def solution(numbers):
+    answer = ""
+    n = [str(x) for x in numbers]
+    n.sort(key = lambda x : x*3 , reverse = True)
+    answer = "".join(n)
+    return answer
+
+numbers = [0,0]
+answer = ""
+n = [str(x) for x in numbers]
+n.sort(key = lambda x : x*3 , reverse = True)
+answer = str(int("".join(n)))
+print(answer)
